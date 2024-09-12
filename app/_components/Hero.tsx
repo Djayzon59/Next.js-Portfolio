e53f5803e64e83e2b2fc9f0e89a87937617b7d6d
Djayzon59 <jasonbailleul59@laposte.net>
2024-09-12 21:37:05 +0200

    homePage

---
+++ app/_components/Hero.tsx
@@ -0,0 +1,23 @@
+import { Section } from "./Section";
+
+export const Hero = () => {
+    return (
+    <Section className="flex max-md:flex-col items-start gap-4">
+        <div className="flex-[3] w-full p-4 flex flex-col gap-2">
+            <h2 className="font-caption text-5xl text-primary">Jason Bailleul</h2>
+            <h3 className="text-3xl font-caption ">Développeur Full Stack</h3>
+            <p>
+                Bienvenue sur mon portfolio ! <br/>
+                Passionné par la programmmation, je vous invite à me découvrir à travers une présentation de mes réalisations ! <br/>
+            </p>
+        </div>
+        <div className="flex-[2] max-md:m-auto ml-auto">
+            <img 
+                src="moir.png"
+                className="w-full h-auto max-w-xs max-md:w-46"
+                alt="jason's picture"
+            />
+        </div>
+    </Section>
+  );
+};
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,13 @@
 import type { Metadata } from "next";
 import { GeistSans } from "geist/font/sans";
+import { GeistMono } from "geist/font/mono";
 import { Anek_Telugu } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
 
 const AnekTelugu = Anek_Telugu({ 
   subsets: ["latin"],
-  variable: "--font-caption"
+  variable: "--font-caption",
 });
 
 export const metadata: Metadata = {
@@ -21,7 +22,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="h-full">
-      <body className={cn(GeistSans.variable, AnekTelugu, "font-sans h-full bg-background text-foreground" )}>
+      <body className={cn(
+              GeistSans.variable, 
+              GeistMono.variable,
+              AnekTelugu.variable, 
+              "font-sans h-full bg-background text-foreground" )}>
         {children}
       </body>
     </html>
