4a38b1882b46d5bc0dd7212484a630855edc68f3
Djayzon59 <jasonbailleul59@laposte.net>
2024-09-12 12:24:56 +0200

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,29 @@
+import type { Metadata } from "next";
+import { GeistSans } from "geist/font/sans";
+import { Anek_Telugu } from "next/font/google";
+import "./globals.css";
+import { cn } from "@/lib/utils";
+
+const AnekTelugu = Anek_Telugu({ 
+  subsets: ["latin"],
+  variable: "--font-caption"
+});
+
+export const metadata: Metadata = {
+  title: "Jason Bailleul . Développeur Full Stack",
+  description: "Développeur Java, React",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" className="h-full">
+      <body className={cn(GeistSans.variable, AnekTelugu, "font-sans h-full bg-background text-foreground" )}>
+        {children}
+      </body>
+    </html>
+  );
+}
