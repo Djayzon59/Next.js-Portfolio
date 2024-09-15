d5b99d0a05b3bbd5d62473d2d7f94de7683d4492
Djayzon59 <jasonbailleul59@laposte.net>
2024-09-15 21:35:32 +0200

    WIP

---
+++ app/_components/Skills.tsx
@@ -0,0 +1,13 @@
+import { Section } from "./Section";
+import { Badge } from "@/components/ui/badge";
+
+export const Skills = () => {
+    return (
+        <Section className="flex flex-col items-start gap-4">
+            <Badge variant={"outline"}>Skills</Badge>
+            <h2 className="scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight first:mt-0">
+                I love working on...
+            </h2>
+        </Section>
+    );
+};
